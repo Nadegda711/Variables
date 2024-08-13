3b6be47cf10e10cb52da14801a7f5616dc1058b2
Nadezda711 <semiliya711@mail.ru>
2024-08-13 19:57:43 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # Hello!
-kvdz = '12'#количество выполненных домашних заданий
-kzh = "1.5"#количество затраченных часов
-nc = "Python"#название курса
-has = "60"#минут в часе
-time_for_1_task = ''
-print(kzh)
-print(kvdz)
-print(nc)
-print(has)
-print(type(kvdz))
-print(float(kzh)*60)#количество затраченных минут
-print((float(kzh)*60)/float(kvdz))#среднее время выполнения в минутах
-print(((float(kzh)*60)/float(kvdz))/float(has))
-print(nc, kvdz, kzh, (((float(kzh)*60)/float(kvdz))/float(has)))
+k_v_d_z = '12'#количество выполненных домашних заданий
+k_z_h = "1.5"#количество затраченных часов
+n_c = "Python"#название курса
+h_a_s = "60"#минут в часе
+time_for_1_task = '0.125'
+print(k_z_h)
+print(k_v_d_z)
+print(n_c)
+print(h_a_s)
+print(type(k_v_d_z))
+print(float(k_z_h)*60)#количество затраченных минут
+print((float(k_z_h)*60)/float(k_v_d_z))#среднее время выполнения в минутах
+print(((float(k_z_h)*60)/float(k_v_d_z))/float(h_a_s))
+print(n_c, 'всего задач: ',k_v_d_z, 'затрачено часов: ',k_z_h, 'среднее время выполнения ',(((float(k_z_h)*60)/float(k_v_d_z))/float(h_a_s)), 'часа.' )
